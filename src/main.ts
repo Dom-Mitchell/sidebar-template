e7f0248a445dda4989d52239701990fb3b5849cd
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-09-05 17:25:35 -0400

    Image Fix

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import "./style.css";
+import './style.css'
 
-// Need to change into TS
-const themeMap = {
-  dark: "light",
-  light: "solar",
-  solar: "dark"
-};
+// Need to change into
+const themeMap: any = {
+  dark: 'light',
+  light: 'solar',
+  solar: 'dark',
+}
 
-const theme = localStorage.getItem('theme');
-const bodyClass = document.body.classList;
-theme && bodyClass.add(theme);
+const theme = localStorage.getItem('theme')
+const bodyClass = document.body.classList
+theme && bodyClass.add(theme)
 
 // Change the theme on a button click
 function toggleTheme() {
-  const current = String(localStorage.getItem('theme'));
-  const next = themeMap[current];
+  const current = localStorage.getItem('theme') as string
+  const next = themeMap[current]
 
-  bodyClass.replace(current, next);
-  localStorage.setItem('theme', next);
+  bodyClass.replace(current, next)
+  localStorage.setItem('theme', next)
 }
 
-document.getElementById('themeButton').onclick = toggleTheme;
+document.getElementById('themeButton').onclick = toggleTheme
